60712bff30afd2c2e08800b52d9ad8eb5c7d7a6a
Ernesto Sola-Thomas <ernesto@solathomas.com>
2025-09-29 02:53:24 +0000

    Merged PR 13: Update URLs to use BASE_URL for dynamic routing and environment handling
    Update URLs to use BASE_URL for dynamic routing and environment handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://huitzo.com',
+  site: process.env.NODE_ENV === 'production' ? 'https://esola-thomas.github.io' : 'http://localhost:4321',
+  base: process.env.NODE_ENV === 'production' ? '/huitzo/' : '/',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
